25aa56928d96cc3236365d5e69c0c067ca8ce185
king <king@qq.com>
2020-02-21 17:25:03 +0800

    nacos java api demo

--- nacos-spring-boot-prop/src/main/java/com/kq/springboot/nacos/NacosSpringBootApplication.java
+++ nacos-spring-boot-prop/src/main/java/com/kq/springboot/nacos/NacosSpringBootApplication.java
@@ -1,5 +1,7 @@
 package com.kq.springboot.nacos;
 
+import com.alibaba.nacos.api.config.ConfigType;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +9,17 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @Slf4j
 @SpringBootApplication
+@NacosPropertySource(dataId = "enabled",groupId = "test",autoRefreshed = true,type = ConfigType.PROPERTIES)
 public class NacosSpringBootApplication {
 
 
     public static void main(String[] args) {
+
+
+
         ConfigurableApplicationContext context = SpringApplication.run(NacosSpringBootApplication.class, args);
+
+        log.info("NacosSpringBootApplication argument= {}",args);
 
         String[] beanNames = context.getBeanDefinitionNames();
 
--- nacos-spring-boot-prop/src/main/java/com/kq/springboot/nacos/controller/ConfigController.java
+++ nacos-spring-boot-prop/src/main/java/com/kq/springboot/nacos/controller/ConfigController.java
@@ -6,6 +6,7 @@ import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -21,10 +22,19 @@ public class ConfigController {
     @NacosValue(value = "${db.password:-1}", autoRefreshed = true)
     private String dbPassword;
 
+    @NacosValue(value = "${enabled:-1}", autoRefreshed = true)
+    private String enabled;
+
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
     public boolean get() {
         return useLocalCache;
+    }
+
+    @RequestMapping(value = "/getEnabled", method = RequestMethod.GET)
+    @ResponseBody
+    public String getEnabled() {
+        return enabled;
     }
 
     @RequestMapping(value = "/getDbPassword", method = RequestMethod.GET)
@@ -42,4 +52,16 @@ public class ConfigController {
 
         return pasword;
     }
+
+    @RequestMapping(value = "/setDbPassword1", method = RequestMethod.GET)
+    @ResponseBody
+    public String setDbPassword1(@RequestParam("password") String password) throws Exception{
+
+        configService.publishConfig("db.password", "test", password);
+
+        String pasword = configService.getConfig("db.password", "test", 3000);
+
+        return pasword;
+    }
+
 }
